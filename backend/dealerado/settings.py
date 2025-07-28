a2827bc304b68e3034ba0db5e2306acbb90f66f1
Sayak Pan <sayakpan9635@gmail.com>
2025-07-28 12:20:43 +0530

    chore: remove unused SQLite database file

--- backend/dealerado/settings.py
+++ backend/dealerado/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'dealerado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB', default='dealerado-dev'),
+        'USER': config('POSTGRES_USER', default='postgres'),
+        'PASSWORD': config('POSTGRES_PASSWORD', default='42541011'),
+        'HOST': config('POSTGRES_HOST', default='localhost'),
+        'PORT': config('POSTGRES_PORT', default='5432'),
     }
 }
 
